88ae7bbb80e336571ab2eb1e7dd8086d395c738e
ktsn <ktsn55@gmail.com>
2016-06-13 00:01:55 +0900

    Remove semi-colons

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
-import assert from 'power-assert';
-import index from '../src/index';
+import assert from 'power-assert'
+import index from '../src/index'
 
 describe('Entry point', () => {
   it('should provide module', () => {
-    assert.deepEqual(index, {});
-  });
-});
+    assert.deepEqual(index, {})
+  })
+})
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,8 +1,8 @@
 /* eslint-env node */
-const conf = require('./webpack.config');
+const conf = require('./webpack.config')
 
-conf.watch = true;
-conf.debug = true;
-conf.devtool = 'source-map';
+conf.watch = true
+conf.debug = true
+conf.devtool = 'source-map'
 
-module.exports = conf;
+module.exports = conf
